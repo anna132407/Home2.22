c4753f72deadf73209d39f0b65fef2ebf308c0ef
anna132407 <anna132407@gmail.com>
2022-07-29 09:38:52 +0300

    Initial commit

---
+++ src/If.java
@@ -0,0 +1,12 @@
+public class If {
+    public static void main(String[] args) {
+        int myInt = 5;
+        if(myInt<10){
+            System.out.println("да, вeрно.");
+        } else if(myInt>20){
+            System.out.println("нeт, нe вeрно.");
+        } else {
+            System.out.println("ни один из предыдущих случаев.");
+        }
+    }
+}
